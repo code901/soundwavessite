2eeba0e19e6176411249d15e85e14240fb717000
Sean McKenna <sean.p.mckenna@icloud.com>
2017-02-19 01:21:53 -0600

    initial commit; scaffold node, express, pug, less and bootstrap

---
+++ app.js
@@ -0,0 +1,28 @@
+var views_path = __dirname + '/src/views/';
+var views_engine = 'pug';
+var landing_view = 'home';
+
+// var css_path = __dirname + '/src/styles/css/';
+var css_request_base = '/css';
+var less_path = __dirname + '/src/styles/less/';
+
+var express = require('express');
+
+var expressLess = require('express-less');
+
+var app = express();
+
+app.use('/css', expressLess(less_path));
+
+app.set('views', views_path);
+app.set('view engine', views_engine);
+
+
+app.get('/', function(req, res) {
+  res.render(landing_view, {
+    title: 'Welcome'
+  });
+});
+
+app.listen(3000);
+
