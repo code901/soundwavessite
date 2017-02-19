2f1e8a09bbf656b8f295407a6608e149b011c845
Sean McKenna <sean.p.mckenna@icloud.com>
2017-02-19 14:26:36 -0600

    added compression middleware

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var views_path = __dirname + '/src/views/';
 var views_engine = 'pug';
 var landing_view = 'home';
-
 // var css_path = __dirname + '/src/styles/css/';
 var css_request_base = '/css';
 var less_path = __dirname + '/src/styles/less/';
 
 var express = require('express');
-
 var expressLess = require('express-less');
+var compression = require('compression');
 
 var app = express();
+app.use(compression());
 
 app.use('/css', expressLess(less_path));
 
@@ -25,4 +25,3 @@ app.get('/', function(req, res) {
 });
 
 app.listen(3000);
-
